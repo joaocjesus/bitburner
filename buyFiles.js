9dbd1472bc037a986ba7e9f95a5a0176cec3e5ae
Joao Jesus <johncjesus@gmail.com>
2021-12-26 12:29:36 +0000

    Updated buyFiles

--- buyFiles.js
+++ buyFiles.js
@@ -1,28 +1,28 @@
 /** @param {NS} ns **/
 
-import { ports, nonPortFiles } from 'lib.js';
-import { enterInTerminal } from 'document.js';
+import { ports, nonPortFiles } from "lib.js";
+import { enterInTerminal } from "document.js";
 
 export async function main(ns) {
-    ports.forEach(({ port, file }) => {
-        const exists = ns.fileExists(file, 'home');
-        ns.print(`${port} port: `);
-        if (exists) {
-            ns.tprint(`File already purchased!`);
-        } else {
-            ns.tprint(`Buying ${file}...`);
-            enterInTerminal(`buy ${file}`);
-        }
-    });
+  ports.forEach(({ port, file }) => {
+    const exists = ns.fileExists(file, "home");
+    ns.print(`${port} port: `);
+    if (exists) {
+      ns.tprint(`'${file}' file already purchased!`);
+    } else {
+      ns.tprint(`Buying ${file}...`);
+      enterInTerminal(`buy ${file}`);
+    }
+  });
 
-    nonPortFiles.forEach(({ name, file }) => {
-        const exists = ns.fileExists(file, 'home');
-        ns.print(`${name}: `);
-        if (exists) {
-            ns.tprint(`File already purchased!`);
-        } else {
-            ns.tprint(`Buying ${file}...`);
-            enterInTerminal(`buy ${file}`);
-        }
-    });
+  nonPortFiles.forEach(({ name, file }) => {
+    const exists = ns.fileExists(file, "home");
+    ns.print(`${name}: `);
+    if (exists) {
+      ns.tprint(`'${file}'' file already purchased!`);
+    } else {
+      ns.tprint(`Buying '${file}' ...`);
+      enterInTerminal(`buy ${file}`);
+    }
+  });
 }
